26b0d16aa4f888e59a7c5273252687506b11b946
Dry-A <112403510+Dry-A@users.noreply.github.com>
2024-10-17 22:29:29 -0300

    Configurando Docker-Compose e o application.properties

---
+++ src/main/java/com/audrey/meublog2024/MeuBlog2024Application.java
@@ -0,0 +1,13 @@
+package com.audrey.meublog2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuBlog2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuBlog2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/audrey/meublog2024/MeuBlog2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audrey.meublog2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuBlog2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
