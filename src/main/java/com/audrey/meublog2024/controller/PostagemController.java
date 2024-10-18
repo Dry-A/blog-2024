cdbaa05eed8a88856712dd4541b4f4422fd74cf0
Dry-A <112403510+Dry-A@users.noreply.github.com>
2024-10-18 01:41:00 -0300

    Adicionado Repository e Controller

--- src/main/java/com/audrey/meublog2024/controller/PostagemController.java
+++ src/main/java/com/audrey/meublog2024/controller/PostagemController.java
@@ -1,4 +1,26 @@
 package com.audrey.meublog2024.controller;
 
+import com.audrey.meublog2024.entities.Postagem;
+import com.audrey.meublog2024.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
+
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Postagem>> getAll(){
+        return ResponseEntity.ok(postagemRepository.findAll());
+    }
 }
--- src/main/java/com/audrey/meublog2024/repository/PostagemRepository.java
+++ src/main/java/com/audrey/meublog2024/repository/PostagemRepository.java
@@ -1,4 +1,10 @@
 package com.audrey.meublog2024.repository;
 
-public interface PostagemRepository {
+import com.audrey.meublog2024.entities.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
 }
