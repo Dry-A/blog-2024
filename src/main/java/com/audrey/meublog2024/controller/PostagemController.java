0d9f383956ba58be57914885faf1827ee2ea3fe1
Dry-A <112403510+Dry-A@users.noreply.github.com>
2024-10-18 14:51:01 -0300

    Adicionando métodos Get

--- src/main/java/com/audrey/meublog2024/controller/PostagemController.java
+++ src/main/java/com/audrey/meublog2024/controller/PostagemController.java
@@ -3,11 +3,10 @@ package com.audrey.meublog2024.controller;
 import com.audrey.meublog2024.entities.Postagem;
 import com.audrey.meublog2024.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +20,20 @@ public class PostagemController {
 
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
+
         return ResponseEntity.ok(postagemRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+
+        return postagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTituloBananinha(@PathVariable String titulo ){
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
     }
 }
--- src/main/java/com/audrey/meublog2024/repository/PostagemRepository.java
+++ src/main/java/com/audrey/meublog2024/repository/PostagemRepository.java
@@ -2,9 +2,15 @@ package com.audrey.meublog2024.repository;
 
 import com.audrey.meublog2024.entities.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+
+    public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
