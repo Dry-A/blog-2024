7da994529a04920f5220a1c6966ad148a06a58d3
Dry-A <112403510+Dry-A@users.noreply.github.com>
2024-10-18 16:39:38 -0300

    Criando Métodos na Controller Categoria

--- src/main/java/com/audrey/meublog2024/repository/CategoriaRepository.java
+++ src/main/java/com/audrey/meublog2024/repository/CategoriaRepository.java
@@ -1,4 +1,15 @@
 package com.audrey.meublog2024.repository;
 
-public interface CategoriaRepository {
+import com.audrey.meublog2024.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
 }
